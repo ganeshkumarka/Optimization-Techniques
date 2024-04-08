62ce2ad0dc54122fc15132251cb333e89ef17d5f
ganeshkumarka <ganeshkumarka03@gmail.com>
2024-04-08 20:26:17 +0530

    m1

--- model/m1.py
+++ model/m1.py
@@ -1,84 +1,67 @@
-# import numpy as np
-
-# def simplex_method(c, A, b, maximize=True):
-#     m, n = A.shape
-#     tableau = np.hstack((A, np.eye(m)))
-#     tableau = np.hstack((tableau, np.vstack((b, np.zeros((m, 1))))))
-#     if maximize:
-#         c_bar = -c
-#     else:
-#         c_bar = c
-
-#     while any(c_bar > 0):
-#         entering_var = np.argmax(c_bar)
-#         ratios = tableau[:, -1] / tableau[:, entering_var]
-#         if all(ratios <= 0):
-#             print("Problem is unbounded.")
-#             return None, None
-#         leaving_var = np.argmin(ratios[ratios > 0])
-        
-#         pivot = tableau[leaving_var, entering_var]
-#         tableau[leaving_var, :] /= pivot
-#         for i in range(m + 1):
-#             if i != leaving_var:
-#                 tableau[i, :] -= tableau[i, entering_var] * tableau[leaving_var, :]
-                
-#         c_bar -= c_bar[entering_var] * tableau[leaving_var, :n]
-
-#     return tableau[:, -1], tableau[-1, -1] if maximize else -tableau[-1, -1]
-
-# if __name__ == "__main__":
-#     # Example input
-#     c = np.array([3, 2])     # Coefficients of the objective function
-#     A = np.array([[1, 1],    # Coefficients of the constraint matrix
-#                   [2, 1]])
-#     b = np.array([4, 5])     # Right-hand side vector
-
-#     # Solve linear optimization problem (maximization)
-#     solution, optimal_value = simplex_method(c, A, b, maximize=True)
-#     print("Maximization:")
-#     print("Optimal solution:", solution)
-#     print("Optimal value:", optimal_value)
-
-#     # Solve linear optimization problem (minimization)
-#     solution, optimal_value = simplex_method(c, A, b, maximize=False)
-#     print("\nMinimization:")
-#     print("Optimal solution:", solution)
-#     print("Optimal value:", optimal_value)
-
 import numpy as np
 
-def simplex(A, b, c):
-    # Initialize the tableau
-    b = b[:, np.newaxis]
-    tableau = np.vstack((np.hstack((A, c)), np.hstack((b, 0))))
-    
-    # Find the pivot column
-    pivot_col = np.argmin(tableau[:-1, -1] / tableau[:-1, :-1])
-    
-    while np.any(tableau[:-1, -1] < 0):
-        # Find the pivot row
-        ratios = tableau[:-1, -1] / tableau[:-1, pivot_col]
-        pivot_row = np.argmin(ratios)
-        
-        # Perform row operations
-        tableau[pivot_row] /= tableau[pivot_row, pivot_col]
-        for i in range(tableau.shape[0]):
-            if i != pivot_row:
-                tableau[i] -= tableau[pivot_row] * tableau[i, pivot_col]
-        
-        # Update the pivot column
-        pivot_col = np.argmin(tableau[:-1, -1] / tableau[:-1, :-1])
-    
-    # Extract the solution
-    x = tableau[:-1, -1]
-    return x
+def create_tableau(c, A, b):
+    m, n = A.shape
+    tableau = np.zeros((m + 1, m + n + 1))
+    tableau[:-1, :-1] = A
+    tableau[:-1, -1] = b
+    tableau[-1, :n] = c
+    return tableau
 
-# Problem data
-A = np.array([[1, 1], [2, 1]])
-b = np.array([10, 15])
-c = np.array([-3, -2]) # Note: We use negative coefficients for minimization
+def select_entering_variable(tableau):
+    return np.argmax(tableau[-1, :-1])
 
-# Solve the problem
-solution = simplex(A, b, c)
-print("Optimal solution:", solution)
+def select_leaving_variable(tableau, entering_variable):
+    ratios = tableau[:-1, -1] / tableau[:-1, entering_variable]
+    return np.argmin(ratios)
+
+def pivot(tableau, key_row, key_col):
+    m, n = tableau.shape
+    key_element = tableau[key_row, key_col]
+    tableau[key_row, :] /= key_element
+    for i in range(m):
+        if i == key_row:
+            continue
+        multiplier = tableau[i, key_col]
+        tableau[i, :] -= multiplier * tableau[key_row, :]
+    return tableau
+
+def is_optimal(tableau):
+    return all(x <= 0 for x in tableau[-1, :-1])
+
+def extract_solution(tableau):
+    m, n = tableau.shape
+    solution = np.zeros(n - 1)
+    basic_variables = np.argmax(tableau[:, :-1], axis=1)
+    for i, j in enumerate(basic_variables):
+        solution[j] = tableau[i, -1]
+    return solution
+
+def simplex(c, A, b):
+    tableau = create_tableau(c, A, b)
+    while not is_optimal(tableau):
+        key_col = select_entering_variable(tableau)
+        key_row = select_leaving_variable(tableau, key_col)
+        tableau = pivot(tableau, key_row, key_col)
+    solution = extract_solution(tableau)
+    return solution
+
+def main():
+    num_variables = int(input("Enter the number of variables: "))
+    num_constraints = int(input("Enter the number of constraints: "))
+
+    c = [float(input(f"Enter coefficient for variable x{i+1} in the objective function: ")) for i in range(num_variables)]
+
+    A = np.zeros((num_constraints,num_variables))
+    print("Enter the coefficients of the constrained equation")
+    for i in range(num_constraints):
+            A[i] = [np.array([float(input(f"enter coeficient for xi+1 for constraoined equation xj+1 "))]) for i in range(num_variables)]
+
+
+    b = [float(input(f"Enter the right-hand side of constraint equation {i+1}: ")) for i in range(num_constraints)]
+
+    solution = simplex(c, A, b)
+    print("Optimal solution:", solution)
+
+if __name__ == "__main__":
+    main()
